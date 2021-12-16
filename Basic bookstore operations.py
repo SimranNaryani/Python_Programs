26e4ebc5b0af357eeef3f265e568f93b7634b86f
Simran Naryani <79084332+SimranNaryani@users.noreply.github.com>
2021-12-16 15:56:57 +0530

    Add files via upload

---
+++ Basic bookstore operations.py
@@ -0,0 +1,47 @@
+print("Basic Bookstore Operations")
+input("a) Delete the duplicate entries\n"
+"b) Display books in ascending order based on cost of books\n"
+"c) Count number of books with cost more than 500\n"
+"d) Copy books in a new list which has cost less than 500.\n")
+n = int(input("Enter number of books:"))
+class library: 
+  name = []
+  bookid = []
+  cost = []
+  def getdata(self):  
+    for i in range(n):
+      self.name.append(input("Enter the name of the book:"))
+      self.bookid.append(int(input("Enter the book id:")))
+      self.cost.append(int(input("Enter the cost of the book:")))
+  def duplicate(self):  
+    templist = []
+    for x in self.name:
+      if x not in templist:
+        templist.append(x)
+    self.name = templist
+    print("List after deleting duplicate values is",templist)
+  def costcheck(self): 
+    cost_less = []
+    cost_high = []
+    for j in self.cost:
+      if(j<500):
+        cost_less.append(j)
+      else:
+        cost_high.append(j)
+    print("The total count of books with price greater than 500 is:",len(cost_high))
+    for x in cost_less:
+      print("Cost of books less than 500 is:",x)
+  def ascending_order(self):
+    self.cost.sort()
+    print("Cost of books in ascending order is",self.cost)
+  def display_data(self):  
+    print("Name \t\t Book id \t Cost")
+    for i in range(n):
+      print(self.name[i],"\t\t",self.bookid[i],"\t\t",self.cost[i])
+l=library()
+l.getdata()
+l.display_data()
+l.ascending_order()
+l.duplicate()
+l.costcheck()
+print("Code by Simran Naryani")
